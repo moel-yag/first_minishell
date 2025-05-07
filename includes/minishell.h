d80435c8ee493fe0225ef3aeae6718723368cfe5
medel <moel-yag@student.1337.ma>
2025-05-07 11:37:33 +0100

    Update minishell.h

--- includes/minishell.h
+++ includes/minishell.h
@@ -21,10 +21,33 @@ typedef struct s_command
     struct s_command *next;
 } t_command;
 
+// Structure for lexer tokens
+typedef struct s_lexer
+{
+    char *value;           // Token value
+    char type;             // Token type (e.g., 'P' for pipe, 'I' for input redirection, etc.)
+    bool in_quotes;        // Whether the token is inside quotes
+    struct s_lexer *next;  // Pointer to the next token
+    struct s_lexer *prev;  // Pointer to the previous token
+} t_lexer;
+
+// Structure for environment variables
+typedef struct s_env
+{
+    int exit_status; // Stores the exit status of the last command
+} t_env;
+
+extern t_env *g_env;
+
 // Function prototypes
 bool is_blank_line(const char *s);
 void print_syntax_error(const char *msg);
 bool validate_syntax(const char *input);
 char **ft_split(const char *str, char delimiter);
+
+// Syntax validation functions
+int pipe_syntax(t_lexer *lex);
+int files_syntax(t_lexer *lex);
+int check_syntax(t_lexer *lex);
 
 #endif // MINISHELL_H
